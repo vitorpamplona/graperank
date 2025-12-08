c7f8a1e6eb78db3c7b5415e6ab38717f1eb37439
Vitor Pamplona <vitor@vitorpamplona.com>
2025-12-08 18:43:53 -0500

    Adds a circular graph test

---
+++ src/test/kotlin/com/vitorpamplona/graperank/v2Stateful/CircularGraph.kt
@@ -0,0 +1,44 @@
+package com.vitorpamplona.graperank.v2Stateful
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class CircularGraph {
+    @Test
+    fun testCircular() = with(Graph()) {
+        val pleb1 = newUser()
+        val pleb2 = newUser()
+        val pleb3 = newUser()
+
+        makeObserver(pleb1)
+
+        assertEquals(1.0, pleb1.scores[pleb1])
+        assertEquals(0.0, pleb1.scores[pleb2])
+        assertEquals(0.0, pleb1.scores[pleb3])
+
+        pleb1 follows pleb2
+        pleb2 follows pleb3
+        pleb3 follows pleb1
+
+        assertEquals(1.0, pleb1.scores[pleb1])
+        assertEquals(0.05394235327440411, pleb1.scores[pleb2])
+        assertEquals(0.001494481751665666, pleb1.scores[pleb3])
+    }
+
+    @Test
+    fun testCircularReverseOrder() = with(Graph()) {
+        val pleb1 = newUser()
+        val pleb2 = newUser()
+        val pleb3 = newUser()
+
+        pleb1 follows pleb2
+        pleb2 follows pleb3
+        pleb3 follows pleb1
+
+        makeObserver(pleb1)
+
+        assertEquals(1.0, pleb1.scores[pleb1])
+        assertEquals(0.05394235327440411, pleb1.scores[pleb2])
+        assertEquals(0.001494481751665666, pleb1.scores[pleb3])
+    }
+}
